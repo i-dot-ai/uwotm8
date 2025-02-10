035522eafb86940559dc65af0b303d5d5b2960c0
Liam Wilkinson <146714503+lmwilkigov@users.noreply.github.com>
2025-02-10 18:08:17 +0000

    Fixing dumb test

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -2,4 +2,4 @@ from uwotm8.convert import convert_american_to_british_spelling
 
 
 def test_convert_american_to_british_spelling():
-    assert convert_american_to_british_spelling("Let's anglicise this text") == "Let's anglicize this text"
+    assert convert_american_to_british_spelling("Let's anglicize this text") == "Let's anglicise this text"
